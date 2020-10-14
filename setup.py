28d451c4a6fac228680bbb44df8904bb427d5224
John Bauer <horatio@gmail.com>
2020-10-14 16:31:53 -0700

    Add some more information to setup.py for uploading the package to pypi.  Includes the default config files (#87)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
   name="elmoformanylangs",
-  version="0.0.2",
+  version="0.0.3.post1",
   packages=setuptools.find_packages(),
   install_requires=[
     "torch",
@@ -12,8 +12,13 @@ setuptools.setup(
     "numpy",
     "overrides",
   ],
+  package_data={'configs': ['elmoformanylangs/configs/*.json']},
+  include_package_data=True,
+  author="哈工大社会计算与信息检索研究中心",
+  description="ELMo, updated to be usable with models for many languages",
+  url="https://github.com/HIT-SCIR/ELMoForManyLangs",
   classifiers=[
-    "Development Status :: 2 - Pre-Alpha",
+    "Development Status :: 3 - Alpha",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3.6",
   ],
