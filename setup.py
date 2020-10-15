b3de5f1dc1ac13638a930b49c41e1f1e0e185ca1
John Bauer <horatio@gmail.com>
2020-10-15 02:07:41 -0700

    Bells and whistle for a pypi release (#88)
    * Use English name.  Set the version to 0.0.4
    
    * Make sure the config files are correctly included every time building a new release
    
    * Include the README as the long description on pypi.  Increment the version number... again

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import setuptools
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setuptools.setup(
   name="elmoformanylangs",
-  version="0.0.3.post1",
+  version="0.0.4.post2",
   packages=setuptools.find_packages(),
   install_requires=[
     "torch",
@@ -14,8 +20,10 @@ setuptools.setup(
   ],
   package_data={'configs': ['elmoformanylangs/configs/*.json']},
   include_package_data=True,
-  author="哈工大社会计算与信息检索研究中心",
+  author="Research Center for Social Computing and Information Retrieval",
   description="ELMo, updated to be usable with models for many languages",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   url="https://github.com/HIT-SCIR/ELMoForManyLangs",
   classifiers=[
     "Development Status :: 3 - Alpha",
