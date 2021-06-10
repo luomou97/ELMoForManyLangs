5028762b23f6e256adcc07aae952d57360d585f5
luomou97 <44665299+luomou97@users.noreply.github.com>
2021-06-10 20:49:13 +0800

    Update token_embedder.py

--- elmoformanylangs/modules/token_embedder.py
+++ elmoformanylangs/modules/token_embedder.py
@@ -8,23 +8,32 @@ import copy
 from .highway import Highway
 
 
+# using [char-lstm-last_hidden_state, word_embedding] as actual embedding, like bilistm-crf
 class LstmTokenEmbedder(nn.Module):
   def __init__(self, config, word_emb_layer, char_emb_layer, use_cuda=False):
+    '''
+    ARGS:
+    : config: model configuration, corresponds to ELMo models options \ref page 2 footnote
+    : word_emb_layer: word embedding layer, defined in embedding_layer.py
+    : char_emb_layer: char embedding layer, defined in embedding_layer.py
+    : used_cude: whether to use cuda
+    '''
     super(LstmTokenEmbedder, self).__init__()
     self.config = config
     self.use_cuda = use_cuda
     self.word_emb_layer = word_emb_layer
     self.char_emb_layer = char_emb_layer
-    self.output_dim = config['encoder']['projection_dim']
-    emb_dim = 0
+    self.output_dim = config['encoder']['projection_dim']   # word embedding dim
+    emb_dim = 0   # acutal embedding dimesion
     if word_emb_layer is not None:
-      emb_dim += word_emb_layer.n_d
+      emb_dim += word_emb_layer.n_d   # n_d, embedding dimension
 
     if char_emb_layer is not None:
       emb_dim += char_emb_layer.n_d * 2
       self.char_lstm = nn.LSTM(char_emb_layer.n_d, char_emb_layer.n_d, num_layers=1, bidirectional=True,
                                batch_first=True, dropout=config['dropout'])
-
+    
+    # combine char-embedding and word-embedding, then project
     self.projection = nn.Linear(emb_dim, self.output_dim, bias=True)
 
   def forward(self, word_inp, chars_inp, shape):
@@ -35,10 +44,10 @@ class LstmTokenEmbedder(nn.Module):
       embs.append(word_emb)
 
     if self.char_emb_layer is not None:
-      chars_inp = chars_inp.view(batch_size * seq_len, -1)
-      chars_emb = self.char_emb_layer(Variable(chars_inp).cuda() if self.use_cuda else Variable(chars_inp))
+      chars_inp = chars_inp.view(batch_size * seq_len, -1)  # flat <batch_size, seq_len, word_len> into <batch_size*seq_len, word_len>
+      chars_emb = self.char_emb_layer(Variable(chars_inp).cuda() if self.use_cuda else Variable(chars_inp))  #<batch_size*seq_len, word_len, d_char>
       _, (chars_outputs, __) = self.char_lstm(chars_emb)
-      chars_outputs = chars_outputs.contiguous().view(-1, self.config['token_embedder']['char_dim'] * 2)
+      chars_outputs = chars_outputs.contiguous().view(-1, self.config['token_embedder']['char_dim'] * 2)  # bidirecional lstm, so last dim*2
       embs.append(chars_outputs)
 
     token_embedding = torch.cat(embs, dim=2)
@@ -49,6 +58,13 @@ class LstmTokenEmbedder(nn.Module):
 class ConvTokenEmbedder(nn.Module):
   def __init__(self, config, word_emb_layer, char_emb_layer, use_cuda):
     super(ConvTokenEmbedder, self).__init__()
+    '''
+    ARGS:
+    : config: model configuration, corresponds to ELMo models options \ref page 2 footnote
+    : word_emb_layer: word embedding layer, defined in embedding_layer.py
+    : char_emb_layer: char embedding layer, defined in embedding_layer.py
+    : used_cude: whether to use cuda
+    '''
     self.config = config
     self.use_cuda = use_cuda
 
@@ -63,7 +79,7 @@ class ConvTokenEmbedder(nn.Module):
     if char_emb_layer is not None:
       self.convolutions = []
       cnn_config = config['token_embedder']
-      filters = cnn_config['filters']
+      filters = cnn_config['filters'] # list of (size, num_filters)
       char_embed_dim = cnn_config['char_dim']
 
       for i, (width, num) in enumerate(filters):
@@ -94,9 +110,9 @@ class ConvTokenEmbedder(nn.Module):
       embs.append(word_emb)
 
     if self.char_emb_layer is not None:
-      chars_inp = chars_inp.view(batch_size * seq_len, -1)
+      chars_inp = chars_inp.view(batch_size * seq_len, -1)  # flat into <batch_size*seq_len, word_len>
 
-      character_embedding = self.char_emb_layer(Variable(chars_inp).cuda() if self.use_cuda else Variable(chars_inp))
+      character_embedding = self.char_emb_layer(Variable(chars_inp).cuda() if self.use_cuda else Variable(chars_inp)) # loop up <batch_size*seq_len, word_len, char_dim>
 
       character_embedding = torch.transpose(character_embedding, 1, 2)
 
@@ -112,14 +128,14 @@ class ConvTokenEmbedder(nn.Module):
       for i in range(len(self.convolutions)):
         convolved = self.convolutions[i](character_embedding)
         # (batch_size * sequence_length, n_filters for this width)
-        convolved, _ = torch.max(convolved, dim=-1)
+        convolved, _ = torch.max(convolved, dim=-1)   # <batch_size*seq_len, num_filter>
         convolved = activation(convolved)
-        convs.append(convolved)
-      char_emb = torch.cat(convs, dim=-1)
-      char_emb = self.highways(char_emb)
+        convs.append(convolved)           # list of <batch_size*seq_len, num_filter>
+      char_emb = torch.cat(convs, dim=-1) # <batch_size*seq_len, num_filters>
+      char_emb = self.highways(char_emb)  # <batch_size*seq_len, num_filters>
 
-      embs.append(char_emb.view(batch_size, -1, self.n_filters))
+      embs.append(char_emb.view(batch_size, -1, self.n_filters))  # <batch_size, seq_len, num_filters>
       
-    token_embedding = torch.cat(embs, dim=2)
+    token_embedding = torch.cat(embs, dim=2)  # <batch_size, seq_len, num_filters + word_dim>
 
-    return self.projection(token_embedding)
+    return self.projection(token_embedding) # project
